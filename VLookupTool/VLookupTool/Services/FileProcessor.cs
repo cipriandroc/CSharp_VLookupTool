73661790e1c56e96724d1fb5f7d3cdb8c3f87592
Ciprian Droc <ciprian.droc@gmail.com>
2023-02-18 13:41:25 -0800

    creating structure

---
+++ VLookupTool/VLookupTool/Services/FileProcessor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VLookupTool.Services
+{
+    internal class FileProcessor
+    {
+
+        //load mainCSV file
+        //load secondary CSV file
+    }
+}
