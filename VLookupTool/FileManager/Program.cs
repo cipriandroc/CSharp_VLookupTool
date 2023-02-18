28040b2ddaff333dfe9ecb2e2a364a3f693ca7a5
Ciprian Droc <ciprian.droc@gmail.com>
2023-02-18 13:39:04 -0800

    init commit

---
+++ VLookupTool/FileManager/Program.cs
@@ -0,0 +1,7 @@
+﻿namespace FileManager
+{
+    public class Program
+    {
+
+    }
+}
---
+++ VLookupTool/VLookupTool/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace VLookupTool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
