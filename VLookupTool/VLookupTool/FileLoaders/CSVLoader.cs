0f43eba93a1ac50d9b30cd3fd57ee6a7b387a177
Ciprian Droc <ciprian.droc@gmail.com>
2023-02-18 23:03:08 -0800

    added a VB csv parser, need to test it

--- VLookupTool/VLookupTool/FileLoaders/CSVLoader.cs
+++ VLookupTool/VLookupTool/FileLoaders/CSVLoader.cs
@@ -1,17 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using Microsoft.VisualBasic.FileIO;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace VLookupTool.FileLoaders
 {
     public class CSVLoader
     {
         public CSVLoader(string fileName) 
-        { 
-            
+        {
+            using (TextFieldParser parser = new TextFieldParser(fileName))
+            {
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(",");
 
+                // Read the headers
+                string[] headers = parser.ReadFields();
+
+                while (!parser.EndOfData)
+                {
+                    // Read a line of data
+                    string[] fields = parser.ReadFields();
+
+                    // Access the fields by their index or header name
+                    string field1 = fields[0]; // Or: string field1 = fields["Column1"]
+                    int field2 = int.Parse(fields[1]); // Or: int field2 = int.Parse(fields["Column2"])
+                }
+            }
         }
 
 
