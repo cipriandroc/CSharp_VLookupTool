69e1f286b171fb72a0b27f417d966a18efaae78a
Ciprian Droc <ciprian.droc@gmail.com>
2023-02-19 12:59:01 -0800

    proof of concept prase and export
    need to resolve issue with cells that contain quotes, implement logic to preserve those, otherwise it pushes the columns if string inside quotes contains comma

--- VLookupTool/VLookupTool/Program.cs
+++ VLookupTool/VLookupTool/Program.cs
@@ -35,20 +35,42 @@ namespace VLookupTool
                 List<string> keysFileA = ExtractDictKeys.Execute(loadFileA[0]);
                 List<string> keysFileB = ExtractDictKeys.Execute(loadFileB[0]);
 
-                string columnFileA = StringFromListSelector.GetString(keysFileA);
-                string columnFileB = StringFromListSelector.GetString(keysFileB);
+                string columnFileA = StringFromListSelector.GetString(keysFileA, "select match column file A");
+                string columnFileB = StringFromListSelector.GetString(keysFileB, "select match column file B");
+                string neededColumn = StringFromListSelector.GetString(keysFileB, "select needed column from file B");
 
-                
+
                 foreach (Dictionary<string, string> rowA in loadFileA) 
                 { 
                     foreach (Dictionary<string, string> rowB in loadFileB) 
                     { 
                         if (rowA[columnFileA] == rowB[columnFileB])
                         {
-                            Console.WriteLine($"found match for:{rowA[columnFileA]}");
+                            Console.WriteLine($"found match: {rowB[neededColumn]}");
+                            if (rowA.ContainsKey(neededColumn))
+                            {
+                                rowA[neededColumn] = rowB[neededColumn];
+                            }
+                            else
+                            {
+                                rowA.Add(neededColumn, rowB[neededColumn]);
+                            }
                         }
                     }
                 }
+
+                List<string> newColumnKeys = ExtractDictKeys.Execute(loadFileA[0]);
+                string header = String.Join(',', newColumnKeys);
+                List<string> parseListOfDictsToStrings = new List<string>();
+                parseListOfDictsToStrings.Add(header);
+
+
+                foreach (Dictionary<string, string> rowA in loadFileA)
+                {
+                    parseListOfDictsToStrings.Add(String.Join(',', rowA.Values));                    
+                }
+
+                System.IO.File.WriteAllLines("parsedFile.csv", parseListOfDictsToStrings);
 
                 //perform match - create new dict with A and B dict
                 //ask user for columns to add from dict B to dict A
--- VLookupTool/VLookupTool/Services/StringFromListSelector.cs
+++ VLookupTool/VLookupTool/Services/StringFromListSelector.cs
@@ -10,11 +10,11 @@ namespace VLookupTool.Services
 {
     public class StringFromListSelector
     {
-        public static string GetString(List<string> list)
+        public static string GetString(List<string> list, string consolePrompt)
         {
             var input = AnsiConsole.Prompt(
                 new SelectionPrompt<string>()
-                    .Title($"[grey]Select a value[/]")
+                    .Title($"[grey]{consolePrompt}[/]")
                     .PageSize(10)
                     .MoreChoicesText("[grey](Move up and down to reveal more objects)[/]")
                     .AddChoices(list)
