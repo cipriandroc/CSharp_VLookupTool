aee288f701fd44afa77ddd39735136bb9315f6ab
Ciprian Droc <ciprian.droc@gmail.com>
2023-02-21 11:11:56 -0800

    Update Program.cs
    moving code blocks to methods

--- VLookupTool/VLookupTool/Program.cs
+++ VLookupTool/VLookupTool/Program.cs
@@ -22,7 +22,7 @@ namespace VLookupTool
                 AnsiConsole.Write(new Markup($"[bold green]Selected[/] [yellow]{fileB}[/]" + "\n"));
             }
 
-            if ((!String.IsNullOrEmpty(fileA)) && (!String.IsNullOrEmpty(fileB))) 
+            if ((!String.IsNullOrEmpty(fileA)) && (!String.IsNullOrEmpty(fileB)))
             {
                 //begin processing
                 List<Dictionary<string, string>> loadFileA = FileManager.Entities.CSVFile.Load(fileA);
@@ -34,97 +34,34 @@ namespace VLookupTool
                 string columnFileA = StringFromListSelector.GetString(keysFileA, "select match column file A");
                 string columnFileB = StringFromListSelector.GetString(keysFileB, "select match column file B");
 
-                List<string> neededColumns = new List<string>();
-                foreach (string key in keysFileB)
-                {
-                    if(key != columnFileB)
-                    {
-                        neededColumns.Add(key);
-                    }
-                }
+                List<string> additionalColumns = GetAdditionalColumns(keysFileB, columnFileB);
 
-                var additionalColumns = AnsiConsole.Prompt(
-                    new MultiSelectionPrompt<string>()
-                        .Title("Select columns from match files to append.")
-                        .PageSize(10)
-                        .MoreChoicesText("[grey](Move up and down to reveal more fruits)[/]")
-                        .InstructionsText(
-                            "[grey](Press [blue]<space>[/] to toggle an item, " +
-                            "[green]<enter>[/] to accept)[/]")
-                        .AddChoices(neededColumns)
-                        );
+                //build match
 
+                PerformVLookup(loadFileA, loadFileB, columnFileA, columnFileB, additionalColumns);
 
-                foreach (Dictionary<string, string> rowA in loadFileA) 
-                { 
-                    foreach (Dictionary<string, string> rowB in loadFileB) 
-                    {
-                        if (rowA[columnFileA] != rowB[columnFileB])
-                        {
-                            continue;
-                        }
-
-                        Console.WriteLine($"found match for : {rowB[columnFileA]}");
-
-                        foreach (string column in additionalColumns)
-                        {
-                            string concatKeyName = String.Concat("vlookup_", column);
-
-                            if (rowA.ContainsKey(concatKeyName))
-                            {
-                                rowA[concatKeyName] = rowB[column];
-                            }
-                            else
-                            {
-                                rowA.Add(concatKeyName, rowB[column]);
-                            }
-                        }
-                    }
-                }
+                //rebuild dict for csv export
 
                 List<string> newColumnKeys = ExtractDictKeys.Execute(loadFileA[0]);
 
                 //parse column keys to identify if any contains comma and add double quotes so CSV doesn't split headers
-                List<string> parseNewColumnKeys = new List<string>();
-                foreach (string key in newColumnKeys)
-                {
-                    string parseKey;
-
-                    if (key.Contains(','))
-                    {
-                        parseKey = '"' + key + '"';
-                    }
-                    else
-                    {
-                        parseKey = key;
-                    }
-
-                    parseNewColumnKeys.Add(parseKey);
-                }
+                List<string> parseNewColumnKeys = ParseCSVColumnsToQuotes(newColumnKeys);
 
                 string header = String.Join(',', parseNewColumnKeys);
 
                 List<string> parseListOfDictsToStrings = new List<string>();
                 parseListOfDictsToStrings.Add(header);
 
-                //parse list of dicts and add double quotes for any value that contains comma so CSV doesn't split column order
-                foreach (Dictionary<string, string> rowA in loadFileA)
-                {
-                    foreach (string key in rowA.Keys)
-                    {
-                        if (rowA[key].Contains(','))
-                        {
-                            rowA[key] = '"' + rowA[key] + '"';
-                        }
-                    }
-                }
+                ParseCSVDictLinesToQuotes(loadFileA);
 
                 //add dict values to list 
                 foreach (Dictionary<string, string> rowA in loadFileA)
                 {
-                    parseListOfDictsToStrings.Add(String.Join(',', rowA.Values));                    
+                    parseListOfDictsToStrings.Add(String.Join(',', rowA.Values));
                 }
 
+
+                //export
                 Console.WriteLine("Select export file location");
                 string exportLocation = FileManager.Program.Start(true);
 
@@ -134,6 +71,97 @@ namespace VLookupTool
                 FileManager.Entities.CSVFile.Save(exportLocation, "parsedFile.csv", parseListOfDictsToStrings);
 
             }
+        }
+
+        private static void ParseCSVDictLinesToQuotes(List<Dictionary<string, string>> loadFileA)
+        {
+            //parse list of dicts and add double quotes for any value that contains comma so CSV doesn't split column order
+            foreach (Dictionary<string, string> rowA in loadFileA)
+            {
+                foreach (string key in rowA.Keys)
+                {
+                    if (rowA[key].Contains(','))
+                    {
+                        rowA[key] = '"' + rowA[key] + '"';
+                    }
+                }
+            }
+        }
+
+        private static List<string> ParseCSVColumnsToQuotes(List<string> newColumnKeys)
+        {
+            List<string> parseNewColumnKeys = new List<string>();
+            foreach (string key in newColumnKeys)
+            {
+                string parseKey;
+
+                if (key.Contains(','))
+                {
+                    parseKey = '"' + key + '"';
+                }
+                else
+                {
+                    parseKey = key;
+                }
+
+                parseNewColumnKeys.Add(parseKey);
+            }
+
+            return parseNewColumnKeys;
+        }
+
+        private static void PerformVLookup(List<Dictionary<string, string>> loadFileA, List<Dictionary<string, string>> loadFileB, string columnFileA, string columnFileB, List<string> additionalColumns)
+        {
+            foreach (Dictionary<string, string> rowA in loadFileA)
+            {
+                foreach (Dictionary<string, string> rowB in loadFileB)
+                {
+                    if (rowA[columnFileA] != rowB[columnFileB])
+                    {
+                        continue;
+                    }
+
+                    Console.WriteLine($"found match for : {rowB[columnFileA]}");
+
+                    foreach (string column in additionalColumns)
+                    {
+                        string concatKeyName = String.Concat("vlookup_", column);
+
+                        if (rowA.ContainsKey(concatKeyName))
+                        {
+                            rowA[concatKeyName] = rowB[column];
+                        }
+                        else
+                        {
+                            rowA.Add(concatKeyName, rowB[column]);
+                        }
+                    }
+                }
+            }
+        }
+
+        private static List<string> GetAdditionalColumns(List<string> keysFileB, string columnFileB)
+        {
+            List<string> neededColumns = new List<string>();
+            foreach (string key in keysFileB)
+            {
+                if (key != columnFileB)
+                {
+                    neededColumns.Add(key);
+                }
+            }
+
+            var additionalColumns = AnsiConsole.Prompt(
+                new MultiSelectionPrompt<string>()
+                    .Title("Select columns from match files to append.")
+                    .PageSize(10)
+                    .MoreChoicesText("[grey](Move up and down to reveal more fruits)[/]")
+                    .InstructionsText(
+                        "[grey](Press [blue]<space>[/] to toggle an item, " +
+                        "[green]<enter>[/] to accept)[/]")
+                    .AddChoices(neededColumns)
+                    );
+            return additionalColumns;
         }
     }
 }
