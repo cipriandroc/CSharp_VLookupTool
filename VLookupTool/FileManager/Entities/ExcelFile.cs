d4999462b8f137cb5a98b6598aa30199087a4635
Ciprian Droc <ciprian.droc@gmail.com>
2023-02-26 19:20:41 -0800

    generalized method

--- VLookupTool/FileManager/Entities/ExcelFile.cs
+++ VLookupTool/FileManager/Entities/ExcelFile.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualBasic.FileIO;
+﻿using System.Collections.Generic;
+using Microsoft.VisualBasic.FileIO;
 
 namespace FileManager.Entities
 {
@@ -14,31 +15,10 @@ namespace FileManager.Entities
 
         public static List<Dictionary<string, string>> FileToDict(string path)
         {
-            var result = new List<Dictionary<string, string>>();
+            List<Dictionary<string, string>> placeholder =
+                new List<Dictionary<string, string>> { new Dictionary<string, string>() };
 
-            using (var parser = new TextFieldParser(path))
-            {
-                parser.CommentTokens = new string[] { "#" };
-                parser.SetDelimiters(new string[] { "," });
-                parser.HasFieldsEnclosedInQuotes = true;
-
-                string[] headers = parser.ReadFields();
-
-                while (!parser.EndOfData)
-                {
-                    string[] fields = parser.ReadFields();
-                    var dict = new Dictionary<string, string>();
-
-                    for (int i = 0; i < headers.Length && i < fields.Length; i++)
-                    {
-                        dict.Add(headers[i], fields[i]);
-                    }
-
-                    result.Add(dict);
-                }
-            }
-
-            return result;
+            return placeholder;
         }
     }
 }
